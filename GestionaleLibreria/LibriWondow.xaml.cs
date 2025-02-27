3811726e5e46dea320a0a42f61bea29b78f77cb6
Santo Caridi <s.caridi@syncpoint.it>
2025-02-27 22:55:43 +0100

    suddivisione gestionale in pagine

---
+++ GestionaleLibreria/LibriWondow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using GestionaleLibreria.Business.Services;
+using GestionaleLibreria.Data;
+using GestionaleLibreria.Data.Models;
+
+
+namespace GestionaleLibreria.WPF
+{
+    public partial class LibriWindow : Window
+    {
+        private readonly LibroService _libroService;
+
+        public LibriWindow()
+        {
+            InitializeComponent();
+            ILibroRepository libroRepository = new LibroRepository();
+            _libroService = new LibroService(libroRepository);
+            CaricaLibri();
+        }
+
+        private void CaricaLibri()
+        {
+            List<Libro> libri = _libroService.GetAllLibri();
+            LibriDataGrid.ItemsSource = libri;
+        }
+
+        private void AggiungiLibro_Click(object sender, RoutedEventArgs e)
+        {
+            var aggiungiFinestra = new AggiungiLibroWindow();
+            aggiungiFinestra.ShowDialog();
+            CaricaLibri();
+        }
+
+        private void ModificaLibro_Click(object sender, RoutedEventArgs e)
+        {
+            if (LibriDataGrid.SelectedItem is Libro libroSelezionato)
+            {
+                var modificaFinestra = new ModificaLibroWindow(libroSelezionato);
+                modificaFinestra.ShowDialog();
+                CaricaLibri();
+            }
+            else
+            {
+                MessageBox.Show("Seleziona un libro per modificarlo.");
+            }
+        }
+
+        private void EliminaLibro_Click(object sender, RoutedEventArgs e)
+        {
+            if (LibriDataGrid.SelectedItem is Libro libroSelezionato)
+            {
+                _libroService.EliminaLibro(libroSelezionato.Id);
+                CaricaLibri();
+            }
+            else
+            {
+                MessageBox.Show("Seleziona un libro per eliminarlo.");
+            }
+        }
+    }
+}
--- GestionaleLibreria/MainWindow.xaml.cs
+++ GestionaleLibreria/MainWindow.xaml.cs
@@ -1,61 +1,19 @@
-﻿using System.Collections.Generic;
-using System.Windows;
-using GestionaleLibreria.Business.Services;
-using GestionaleLibreria.Data;
-using GestionaleLibreria.Data.Models;
+﻿using System.Windows;
 
 namespace GestionaleLibreria.WPF
 {
     public partial class MainWindow : Window
     {
-        private readonly LibroService _libroService;
-
         public MainWindow()
         {
             InitializeComponent();
-            ILibroRepository libroRepository = new LibroRepository();
-            _libroService = new LibroService(libroRepository);
-            CaricaLibri();
         }
 
-        private void CaricaLibri()
+        private void GestioneLibri_Click(object sender, RoutedEventArgs e)
         {
-            List<Libro> libri = _libroService.GetAllLibri();
-            LibriDataGrid.ItemsSource = libri;
-        }
-
-        private void AggiungiLibro_Click(object sender, RoutedEventArgs e)
-        {
-            var aggiungiFinestra = new AggiungiLibroWindow();
-            aggiungiFinestra.ShowDialog();
-            CaricaLibri();
-        }
-
-        private void ModificaLibro_Click(object sender, RoutedEventArgs e)
-        {
-            if (LibriDataGrid.SelectedItem is Libro libroSelezionato)
-            {
-                var modificaFinestra = new ModificaLibroWindow(libroSelezionato);
-                modificaFinestra.ShowDialog();
-                CaricaLibri();
-            }
-            else
-            {
-                MessageBox.Show("Seleziona un libro per modificarlo.");
-            }
-        }
-
-        private void EliminaLibro_Click(object sender, RoutedEventArgs e)
-        {
-            if (LibriDataGrid.SelectedItem is Libro libroSelezionato)
-            {
-                _libroService.EliminaLibro(libroSelezionato.Id);
-                CaricaLibri();
-            }
-            else
-            {
-                MessageBox.Show("Seleziona un libro per eliminarlo.");
-            }
+            // Apri la finestra dedicata alla gestione dei libri
+            LibriWindow libriWindow = new LibriWindow();
+            libriWindow.ShowDialog();
         }
 
         private void GestioneClienti_Click(object sender, RoutedEventArgs e)
