952d447ebbbc77832eb588f250e2f973cbae0848
Santo Caridi <s.caridi@syncpoint.it>
2025-03-11 09:44:49 +0100

    modifica model

---
+++ GestionaleLibreria.Data/Migrations/202503110832493_ModificaModelloCliente.cs
@@ -0,0 +1,24 @@
+﻿namespace GestionaleLibreria.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ModificaModelloCliente : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Clientes", "Nome", c => c.String(nullable: false, maxLength: 50));
+            AlterColumn("dbo.Clientes", "Cognome", c => c.String(nullable: false, maxLength: 50));
+            AlterColumn("dbo.Clientes", "Email", c => c.String(nullable: false));
+            AlterColumn("dbo.Clientes", "Telefono", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Clientes", "Telefono", c => c.String());
+            AlterColumn("dbo.Clientes", "Email", c => c.String());
+            AlterColumn("dbo.Clientes", "Cognome", c => c.String(nullable: false));
+            AlterColumn("dbo.Clientes", "Nome", c => c.String(nullable: false));
+        }
+    }
+}
--- GestionaleLibreria.Data/Models/Cliente.cs
+++ GestionaleLibreria.Data/Models/Cliente.cs
@@ -7,15 +7,20 @@ namespace GestionaleLibreria.Data.Models
         [Key]
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Il nome è obbligatorio.")]
+        [MaxLength(50, ErrorMessage = "Il nome non può superare i 50 caratteri.")]
         public string Nome { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Il cognome è obbligatorio.")]
+        [MaxLength(50, ErrorMessage = "Il cognome non può superare i 50 caratteri.")]
         public string Cognome { get; set; }
 
-        [EmailAddress]
+        [Required(ErrorMessage = "L'email è obbligatoria.")]
+        [EmailAddress(ErrorMessage = "Inserisci un'email valida.")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "Il numero di telefono è obbligatorio.")]
+        [RegularExpression(@"^\d{8,15}$", ErrorMessage = "Il telefono deve contenere solo numeri (8-15 cifre).")]
         public string Telefono { get; set; }
     }
 }
